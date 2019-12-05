abec6adb05d11d539b6559d2d1d216296e1be6c2
Steve Casey <stevencasey21@gmail.com>
2019-12-04 21:05:15 -0500

    filled in description/url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject wavefunctioncollapse "0.1.4"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject wavefunctioncollapse "0.1.5"
+  :description "Bitmap & tilemap generation from a single example with the help of ideas from quantum mechanics."
+  :url "https://github.com/sjcasey21/wavefunctioncollapse-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
