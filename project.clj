66e4200a15c9866695a2e223f9b283c22daf94f6
Steve Casey <stevencasey21@gmail.com>
2019-12-18 23:30:31 -0500

    bump dependency version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject wavefunctioncollapse "0.1.5"
+(defproject wavefunctioncollapse "0.1.6"
   :description "Bitmap & tilemap generation from a single example with the help of ideas from quantum mechanics."
   :url "https://github.com/sjcasey21/wavefunctioncollapse-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.github.sjcasey21/wavefunctioncollapse "0.2.1"]]
+                 [com.github.sjcasey21/wavefunctioncollapse "0.2.2"]]
   :plugins [[lein-codox "0.10.7"]]
   :profiles {:dev {:dependencies [[cheshire "5.9.0"]
                                   [net.mikera/imagez "0.12.0"]]}}
